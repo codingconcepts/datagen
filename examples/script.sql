9faee75d90cde8e286790b5186b748dbbcdb9cd7
Coding Concepts <codingconcepts@users.noreply.github.com>
2019-05-28 14:42:57 +0300

    Update script.sql

--- examples/script.sql
+++ examples/script.sql
@@ -4,7 +4,7 @@ insert into "owner" ("name", "date_of_birth") values
 {{range $i, $e := $.times_1000 }}
 	{{if $i}},{{end}}
 	(
-		'{{stringf "%s.%s@lush.co.uk" 5 5 "abcdefg" 5 5 "hijklmnop" }}',
+		'{{stringf "%s.%s@acme.co.uk" 5 5 "abcdefg" 5 5 "hijklmnop" }}',
 		'{{date "1900-01-01" "now" ""}}'
 	)
 {{end}}
